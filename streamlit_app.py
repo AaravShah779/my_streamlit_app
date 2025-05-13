6345b2598a6769c950f4a53a6edaf7fb1ef0c4e8
AaravShah779 <779aaravshah@gmail.com>
2025-05-13 21:07:42 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="My First App", layout="centered")
+
+st.title("🎈 Hello Streamlit!")
+st.write("This is a basic Streamlit web app.")
+
+name = st.text_input("Enter your name:")
+if name:
+    st.success(f"Hello, {name} 👋")
