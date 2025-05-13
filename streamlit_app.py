0a430d3afa22a05eb846074be87fcaa40e5c8bcb
AaravShah779 <779aaravshah@gmail.com>
2025-05-13 22:20:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,7 @@ st.write("This is a basic Streamlit web app.")
 name = st.text_input("Enter your name:")
 if name:
     st.success(f"Hello, {name} 👋")
-    # --- Footer ---
-st.markdown("""
-<hr style="margin-top: 2em; margin-bottom: 1em;">
 
-<div style='text-align: center; color: gray;'>
-    Made with ❤️ by Aarav Shah
-</div>
-""", unsafe_allow_html=True)
+ # Footer
+st.markdown("---")
+st.markdown("Made with ❤️ by Aarav Shah")
